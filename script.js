cfeb33f5a7757a97f8d35d469f5ce38dbfadb062
DEEPUKRISHNA KN <100841562+Deepukrishnakn@users.noreply.github.com>
2022-04-18 09:19:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function chat(){
+    alert("Lets Chat Now")
+    window.location=("https://www.pepsodent.in/digidentist.html")
+}
+function signup(){
+    alert("Lets sign up")
+    window.location=("https://www.pepsodent.in/sign-up.html")
+}
+function viewmore(){
+    
+    window.location=("https://www.pepsodent.in/dental-health-tips.html")
+}
